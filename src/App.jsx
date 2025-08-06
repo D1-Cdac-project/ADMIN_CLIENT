01a9725c13abdf3f66c8b376f5bd9bede630be7a
Akshay Londhe <akshaylondhe3399@gmail.com>
2025-08-06 15:15:46 +0530

    adding admin login and provider registration

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { Toaster } from "react-hot-toast";
+// Auth Pages
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ProviderRegister from "./pages/ProviderRegister";
 
 function App() {
   return (
     <>
+     <Routes>
+        {/* Auth Routes */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/register/provider" element={<ProviderRegister />} />
+      </Routes>
       <Toaster
         position="top-right"
         toastOptions={{
