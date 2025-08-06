354f3783474666b3f06f8f5f0b06414bca388491
krh1102002 <hulke1102002@gmail.com>
2025-08-06 15:05:07 +0530

    adding admin layout and sidebar

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Toaster } from "react-hot-toast";
+
+function App() {
+  return (
+    <>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: "#363636",
+            color: "#fff",
+          },
+        }}
+      />
+    </>
+  );
+}
+
+export default App;
